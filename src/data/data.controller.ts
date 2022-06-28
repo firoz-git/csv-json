24bc80d7326f70177e42080a1719646913d3acde
firos khan <firoskhansha@gmail.com>
2022-06-28 14:45:18 +0530

    final commit

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -9,11 +9,9 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { DataService } from './data.service';
-import { csvToJson } from 'convert-csv-to-json';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CsvToJsonDto } from './csv.dto';
 import { diskStorage } from 'multer';
-import csv from 'csvtojson';
 
 @Controller('data')
 export class DataController {
@@ -28,20 +26,5 @@ export class DataController {
   async convertData(@Req() req, @Query() query: any, @UploadedFile() file) {
     console.log(file);
     return await this.dataService.convertData(file);
-    // const csvPath = `files/${file.filename}`;
-
-    // console.log(result);
-    // res.send(result);
   }
-  @Post('/check')
-  async checkData(@Req() { csv }: CsvToJsonDto) {
-    console.log(csv);
-
-    // return await this.dataService.checkData(csv);
-  }
-  // @Post('/simple')
-  // async simpleTest(): Promise<any> {
-  //   const csvPath = `files/${files}`;
-  //   const jsonArray = await csv().fromFile();
-  // }
 }
--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import csvToJson from 'convert-csv-to-json';
 import { readFileSync } from 'fs';
-import { readFile } from 'fs/promises';
-import * as csv2json from 'csv2json';
-import * as converter from 'json-2-csv';
-import csv from 'csvtojson';
 
 @Injectable()
 export class DataService {
@@ -12,40 +7,21 @@ export class DataService {
     const csvFile = readFileSync(`files/${file.filename}`);
     const csvData = csvFile.toString().split('\n');
     const header = csvData[0].split(',');
-    console.log(header[0], csvData.length);
-    for (let i = 1; i <= csvData.length; i++) {
-      const str = csvData[i];
-      console.log(str);
+    console.log(header);
+
+    const jsonObj: any[] = [];
+    for (let i = 1; i < csvData.length; i++) {
+      const str = csvData[i].split(',');
+      console.log(str, str.length, 'here iam');
+
+      const obj = {};
+      for (let j = 0; j < str.length; j++) {
+        obj[header[j].trim()] = str[j].trim();
+      }
+      jsonObj.push(obj);
     }
-
-    // console.log(csvData[0].split(','));
-    // const result = [];
-    // const headers = csvData[0].split(', ');
-    // for (let i = 1; i < csvData.length - 1; i++) {
-    //   let obj = {};
-    // }
-    // console.log(headers);
-
-    // const csvPath = `file/${file.filename}`;
-    // csv()
-    //   .fromFile(csvPath)
-    //   .then((jsonObj) => {
-    //     console.log(jsonObj);
-    //   });
-    // const jsonArray = await csv().fromFile(csvPath);
-    // console.log(jsonArray);
-
-    // const lines = csvData.split('/n');
-    // const keys = lines[0].split(',');
-    // console.log(keys);
-
-    // const json = csvToJson.getJsonFromCsv('myInputFile.csv');
-    // for (let i = 0; i < json.length; i++) {
-    //   console.log(json[i]);
-    // }
-  }
-  async checkData(data: string) {
-    const result = await converter.csv2jsonAsync(data);
-    console.log(result);
+    JSON.stringify(jsonObj);
+    return await jsonObj;
+    console.log(jsonObj, 'iam jsonobj');
   }
 }
